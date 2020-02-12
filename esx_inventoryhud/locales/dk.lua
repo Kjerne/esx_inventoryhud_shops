49a571214ae04f8ca0e6ce12ca390b6471f8845c
kevi2796 <59027271+kevi2796@users.noreply.github.com>
2020-02-12 03:57:52 +0100

    Danish translation

---
+++ esx_inventoryhud/locales/dk.lua
@@ -0,0 +1,12 @@
+
+Locales["dk"] = {
+	["cash"] = "Penge",
+	["black_money"] = "Sorte Penge",
+	["player_nearby"] = "Denne spiller er ikke tæt på dig mere!",
+	["players_nearby"] = "Der er ikke nogle spillere tæt på dig!",
+	["openinv_help"] = "Åbner tasken på en anden spiller",
+	["openinv_id"] = "Spiller ID",
+	["no_permissions"] = "Du har ikke tilladelse til at gøre dette!",
+	["no_player"] = "Fandt ikke en spiller med det givende ID!",
+	["player_inventory"] = "Spiller Taske"
+}
